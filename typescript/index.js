63a1072423b9833652b930b976b72fb208a4d973
Jony-kh <jonykhurramov@gmail.com>
2021-06-26 00:20:28 +0500

    hello

--- typescript/index.js
+++ typescript/index.js
@@ -63,3 +63,13 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+var counter = 0;
+
+function timer() {
+  counter++;
+  document.getElementById("count").innerHTML = counter;
+  setTimeout("timer()", 1000);
+}
